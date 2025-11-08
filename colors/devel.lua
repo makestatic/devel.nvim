dec9b2892540da5c686d7b080cb0ef6bbb251d5c
ali. <alielmalki.developer@gmail.com>
2025-11-08 14:34:53 +0000

    adding tree-sitter support

--- colors/devel.lua
+++ colors/devel.lua
@@ -1,4 +1,4 @@
--- devel (1.2)
+-- devel (1.3)
 -- https://github.com/makestatic/devel.nvim
 
 local c = {
@@ -13,17 +13,16 @@ local c = {
     brown      = "#A0522D",
 }
 
-
 vim.cmd("hi clear")
 if vim.fn.exists("syntax_on") == 1 then
     vim.cmd("syntax reset")
 end
-vim.g.colors_name = "dnvim"
+vim.g.colors_name = "devel"
 vim.o.termguicolors = true
 
 local groups = {
     Normal         = { fg = c.white, bg = c.black },
-    Cursor             = { fg = c.white, bg = c.black },
+    Cursor         = { fg = c.white, bg = c.black },
     CursorLine     = { bg = c.black },
     LineNr         = { fg = c.brown, bg = c.black },
     CursorLineNr   = { fg = c.brown, bg = c.black },
@@ -32,54 +31,95 @@ local groups = {
     StatusLine     = { fg = c.white, bg = c.black },
     StatusLineNC   = { fg = c.gray, bg = c.black },
     Comment        = { fg = c.blue },
-    Keyword        = { fg = c.brown },
-    Statement      = { fg = c.brown },
-    Conditional    = { fg = c.brown },
-    Repeat         = { fg = c.brown },
-    Label          = { fg = c.brown },
-    Exception      = { fg = c.brown },
-    KeywordFunction= { fg = c.brown },
-    Type           = { fg = c.green },
-    TypeBuiltin    = { fg = c.magenta },
-    String         = { fg = c.red },
-    Number         = { fg = c.red },
-    Boolean        = { fg = c.red },
-    Constant       = { fg = c.magenta },
-    ConstantBuiltin= { fg = c.magenta },
-    Identifier     = { fg = c.white },
-    IdentifierBuiltin= { fg = c.magenta },
-    Function       = { fg = c.white },
-    FunctionBuiltin= { fg = c.magenta },
-    Operator       = { fg = c.white },
-    SpecialChar    = { fg = c.magenta },
-    Special        = { fg = c.white },
-    PreProc        = { fg = c.magenta },
-    Include        = { fg = c.magenta },
-    Define         = { fg = c.magenta },
-    Macro          = { fg = c.magenta },
-    Todo           = { fg = c.magenta, bold = true },
+    Visual         = { bg = c.gray },
+    Search         = { fg = c.black, bg = c.brown },
+    IncSearch      = { fg = c.black, bg = c.green },
+    Pmenu          = { fg = c.white, bg = c.black },
+    PmenuSel       = { fg = c.brown, bg = c.black },
+    PmenuSbar      = { bg = c.black },
+    PmenuThumb     = { bg = c.gray },
+    EndOfBuffer    = { fg = c.black },
+    MatchParen     = { bg = c.magenta, fg = c.black, bold = true },
+    Keyword            = { fg = c.brown },
+    Statement          = { fg = c.brown },
+    Conditional        = { fg = c.brown },
+    Repeat             = { fg = c.brown },
+    Label              = { fg = c.brown },
+    Exception          = { fg = c.brown },
+    KeywordFunction    = { fg = c.brown },
+    Type               = { fg = c.green },
+    TypeBuiltin        = { fg = c.green },
+    String             = { fg = c.red },
+    Character          = { fg = c.red },
+    Number             = { fg = c.red },
+    Boolean            = { fg = c.red },
+    Constant           = { fg = c.magenta },
+    ConstantBuiltin    = { fg = c.magenta },
+    Identifier         = { fg = c.white },
+    IdentifierBuiltin  = { fg = c.magenta },
+    Function           = { fg = c.white },
+    FunctionBuiltin    = { fg = c.magenta },
+    Operator           = { fg = c.white },
+    SpecialChar        = { fg = c.magenta },
+    Special            = { fg = c.white },
+    PreProc            = { fg = c.magenta },
+    Include            = { fg = c.magenta },
+    Define             = { fg = c.magenta },
+    Macro              = { fg = c.magenta },
+    Todo               = { fg = c.magenta, bold = true },
     Delimiter          = { fg = c.white },
-    DelimiterParen     = { fg = c.white },
-    DelimiterBracket   = { fg = c.white },
-    DelimiterBrace     = { fg = c.white },
-    OperatorParen      = { fg = c.white },
-    Visual             = { bg = c.gray },
-    Search             = { fg = c.black, bg = c.brown },
-    IncSearch          = { fg = c.black, bg = c.green },
-    Pmenu              = { fg = c.white, bg = c.black },
-    PmenuSel           = { fg = c.brown, bg = c.black },
-    PmenuSbar          = { bg = c.black },
-    PmenuThumb         = { bg = c.gray },
-    EndOfBuffer        = { fg = c.black },
-    MatchParen         = { bg = c.magenta, fg = c.black, bold = true },
 }
 
 for group, opts in pairs(groups) do
     vim.api.nvim_set_hl(0, group, opts)
 end
 
--- who needs treesittah?
-local function disableTS()
-	vim.cmd("TSDisable highlight")
+local ts = {
+    ["@keyword"]                = "Keyword",
+    ["@keyword.function"]       = "KeywordFunction",
+    ["@keyword.return"]         = "Keyword",
+    ["@keyword.repeat"]         = "Repeat",
+    ["@keyword.conditional"]    = "Conditional",
+    ["@keyword.import"]         = "Include",
+    ["@keyword.directive"]      = "PreProc",
+    ["@type"]                   = "Type",
+    ["@type.builtin"]           = "TypeBuiltin",
+    ["@type.definition"]        = "Type",
+    ["@type.qualifier"]         = "Type",
+    ["@constant"]               = "Constant",
+    ["@constant.builtin"]       = "ConstantBuiltin",
+    ["@constant.macro"]         = "Macro",
+    ["@boolean"]                = "Boolean",
+    ["@number"]                 = "Number",
+    ["@float"]                  = "Number",
+    ["@string"]                 = "String",
+    ["@string.escape"]          = { fg = c.magenta },
+    ["@string.special"]         = "String",
+    ["@character"]              = "Character",
+    ["@function"]               = "Function",
+    ["@function.call"]          = "Function",
+    ["@function.builtin"]       = "FunctionBuiltin",
+    ["@method"]                 = "Function",
+    ["@constructor"]            = "Function",
+    ["@field"]                  = "Identifier",
+    ["@variable"]               = "Identifier",
+    ["@variable.builtin"]       = "IdentifierBuiltin",
+    ["@operator"]               = "Operator",
+    ["@punctuation.delimiter"]  = "Delimiter",
+    ["@punctuation.bracket"]    = "Delimiter",
+    ["@punctuation.special"]    = "SpecialChar",
+    ["@preproc"]                = "PreProc",
+    ["@include"]                = "Include",
+    ["@define"]                 = "Define",
+    ["@macro"]                  = "Macro",
+    ["@comment"]                = "Comment",
+    ["@comment.todo"]           = "Todo",
+}
+
+for group, link in pairs(ts) do
+    if type(link) == "string" then
+        vim.api.nvim_set_hl(0, group, { link = link })
+    else
+        vim.api.nvim_set_hl(0, group, link)
+    end
 end
-vim.schedule(disableTS)
